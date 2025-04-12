6bc3978f689861f2960705a3207f97a7483512e0
AtnProjects <72979783+AtnProjects@users.noreply.github.com>
2025-04-12 23:16:18 +0000

    Add files via upload

---
+++ NixOS-Hyprland/hosts/rootkit/home/programs/brave/default.nix
@@ -0,0 +1,10 @@
+  programs.chromium = {
+    enable = true;
+    package = pkgs.brave;
+    extensions = [
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
+    ];
+    commandLineArgs = [
+      "--disable-features=WebRtcAllowInputVolumeAdjustment"
+    ];
+  }
---
+++ NixOS-Hyprland/hosts/rootkit/import.nix
@@ -0,0 +1,12 @@
+{
+  imports = [
+
+    ./home/programs/chormium/default.nix
+     ./home/Services/wine/default.nix
+    ./home/programs/sunshine/default.nix
+    ./hosts-desktop/virtualisation/default.nix
+    ./home/home.nix
+    ./home/programs/steam/default.nix  
+ #  ./home/programs/vscodium/default.nix
+  ];
+}

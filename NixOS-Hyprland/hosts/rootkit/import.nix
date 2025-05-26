dd7de4e8490dd87ba75a987eb3f51b1294cc9746
AtnProjects <72979783+AtnProjects@users.noreply.github.com>
2025-05-26 08:53:14 +0000

    Add files via upload

---
+++ NixOS-Hyprland/hosts/rootkit/import.nix
@@ -0,0 +1,13 @@
+{
+  imports = [
+    ./home/programs/chormium/default.nix
+    # ./home/Services/wine/default.nix
+    ./home/programs/sunshine/default.nix
+    ./Host-desktop/Virtualbox/default.nix
+    ./home/home.nix
+    # ./home/programs/steam/default.nix 
+    ./Host-desktop/virtmanager
+./home/programs/vscode/default.nix 
+  
+  ];
+}
